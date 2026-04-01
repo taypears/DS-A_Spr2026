8855b501befa64d77941090429cb8958ff23e60a
T Pearson <p.tayeja@gmail.com>
2026-03-31 23:31:18 -0400

    modifications to resolve problems

--- Lab-5/Lab-5/Main.java
+++ Lab-5/Lab-5/Main.java
@@ -20,21 +20,35 @@ class Main {
     // Step 3 - Delete the value 17
     lab5Tree.delete(17);
     // Step 4 - Traverse and output the values using inorder (sorted)
-    System.out.println("Inorder (sorted): " + lab5Tree.inorder());
+    System.out.print("Inorder (sorted): ");
+    lab5Tree.inorder();
+
 
     // Step 5 - Traverse and output the values using postorder
-    System.out.println("Postorder: " + lab5Tree.postorder());
+    System.out.print ("\nPostorder: ");
+    lab5Tree.postorder();
 
     // Step 6 - Traverse and output the values using preorder
-    System.out.println("Preorder:" + lab5Tree.preorder());
+    System.out.print("\nPreorder:");
+    lab5Tree.preorder();
     // Step 7 - Display the result of a search for the value 36
-    System.out.println("Search 36: " + lab5Tree.search(36));
+    System.out.println("\nSearch 36: " + lab5Tree.search(36));
 
     // Step 8 - Display the result of a search for the value 37
-    System.out.println("Search 37:" + lab5Tree.search(37));
+    System.out.println("\nSearch 37:" + lab5Tree.search(37));
     // Step 9 - Using the path() method, display the path from the root to 2
-    System.out.println("Path from root to 2: " + lab5Tree.path(2));
+    System.out.print("\nPath from root to 2: ");
+    java.util.ArrayList<BST.TreeNode<Integer>> path2 = lab5Tree.path(2);
+
+    for (int i = 0; path2 != null && i < path2.size(); i++) {
+        System.out.print(path2.get(i).element + " ");
+    }
     // Step 10 - Display the path from the root to 34
-    System.out.println("Path from root to 34 :" + lab5Tree.path(34));
+    System.out.print("\nPath from root to 34: ");
+    java.util.ArrayList<BST.TreeNode<Integer>> path34 = lab5Tree.path(34);
+    
+    for (int i = 0; path34 != null && i < path34.size(); i++) {
+        System.out.print(path34.get(i).element + " ");
+    }
   }
 }
