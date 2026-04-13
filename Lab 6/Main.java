6e04146ef8edafdcac15645f9f99b5d1e1f5474b
T Pearson <p.tayeja@gmail.com>
2026-04-12 21:56:10 -0400

    adjustments

--- Lab 6/Main.java
+++ Lab 6/Main.java
@@ -1,6 +1,34 @@
 class Main {
   public static void main(String[] args) {
-    // Use the instructions in Blackboard or instructions.md to complete Lab 6
-    
+    // create a map called creditHours
+    MyHashMap<String, Integer> creditHours = new MyHashMap<>();
+
+    // Step 4: Insert Values
+    creditHours.put("IT-1025", 3);
+    creditHours.put("IT-1050", 3);
+    creditHours.put("IT-1150", 3);
+    creditHours.put("IT-2310", 3);
+    creditHours.put("IT-2320", 4);
+    creditHours.put("IT-2351", 4);
+    creditHours.put("IT-2650", 4);
+    creditHours.put("IT-2660", 4);
+    creditHours.put("IT-2030", 4);
+
+    // Step 5: Check for the following values. Display true or false depending on
+    // whether or not the map has the key.
+
+    System.out.println("Contains key IT-1025: " + creditHours.containsKey("IT-1025"));
+    System.out.println("Contains key IT-2110: " + creditHours.containsKey("IT-2110"));
+
+    // Step 6: Print all of the values in the map
+    System.out.println("Values in the map: " + creditHours.values());
+
+    // Step 7: Remove IT-2030 and IT-1150
+    creditHours.remove("IT-2030");
+    creditHours.remove("IT-1150");
+
+    // Step 8: Print all of the values in the map
+    System.out.println("Values in the map after removal: " + creditHours.values());
+
   }
 }
--- Lab 6/MyHashMap.java
+++ Lab 6/MyHashMap.java
@@ -22,99 +22,192 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     // Hash table is an array with each cell that is a linked list
     LinkedList<MyMap.Entry<K,V>>[] table;
 
-        //Construct a map with the default capacity and load factor
-        public MyHashMap() {
+    //Construct a map with the default capacity and load factor
+    public MyHashMap() {
         this(DEFAULT_INITIAL_CAPACITY, DEFAULT_MAX_LOAD_FACTOR);
-        }
-
-        //Construct a map with the specified initial capacity and default load factor
-        public MyHashMap(int initialCapacity) {
-        this(initialCapacity, DEFAULT_MAX_LOAD_FACTOR);
-        }
-
-        // Construct a map with the specified initial capacity and load factor
-        public MyHashMap(int initialCapacity, float loadFactorThreshold) {
-            if (initialCapacity > MAXIMUM_CAPACITY)
-                this.capacity = MAXIMUM_CAPACITY;
-            else
-                this.capacity = trimToPowerOf2(initialCapacity);
-                this.loadFactorThreshold = loadFactorThreshold;
-                table = new LinkedList[capacity];
-            }
-
-        @Override // Remove all of the entries from this map
-
-        public void clear() {
-            size = 0;
-            removeEntries();
-        }
-
-        @Override //Return true if the specified key is in the map
-        public boolean containsKey(K key) {
-            if (get(key) != null)
-                return true;
-            else
-                return false;
-        }
-        
-        @Override /** Return true if this map contains the value */
-        public boolean containsValue(V value) {
-            for (int i = 0; i < capacity; i++) {
-                if (table[i] != null) {
-                    LinkedList<Entry<K, V>> bucket = table[i];
-                    for (Entry<K, V> entry: bucket)
-                        if (entry.getValue().equals(value))
-                            return true;
-                }
-            }
-
-            return false;
-        }
-
-        public V get(K key) {
-            int bucketIndex = hash(key.hashCode());
-            if (table[bucketIndex] != null) {
-                LinkedList<Entry<K, V>> bucket = table[bucketIndex];
-                for (Entry<K, V> entry: bucket)
-                    if (entry.getKey().equals(key))
-                        return entry.getValue(); 
-            }
-            return null;
-        }
-            @Override /** Return true if this map contains no entries */
-            public boolean isEmpty() {
-            return size == 0;
-            }
-
-            @Override //Return a set consisting of the keys in this map
-            public java.util.Set<K> keySet() {
-                java.util.Set<K> set = new java.util.HashSet<>();
-                    for (int i = 0; i < capacity; i++) {
-                        if (table[i] != null) {
-                            LinkedList<Entry<K, V>> bucket = table[i];
-                            for (Entry<K, V> entry: bucket)
-                                set.add(entry.getKey()); }}
-            return set;}
-
-            @Override // Add an entry (key, value) into the map
-
-            public V put(K key, V value) {
-                if (get(key) != null) { // The key is already in the map
-                    int bucketIndex = hash(key.hashCode());
-                    LinkedList<Entry<K, V>> bucket = table[bucketIndex];
-                        for (Entry<K, V> entry: bucket)
-                        if (entry.getKey().equals(key)) {
-                            V oldValue = entry.getValue();
-
-                            // Replace old value with new value
-                            entry.value = value;
-
-                            // Return the old value for the key
-                            return oldValue;
-                        }
-                }
-                // Continue with adding new entry logic...
-            }
-
-        
     }
+
+    //Construct a map with the specified initial capacity and default load factor
+    public MyHashMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_MAX_LOAD_FACTOR);
+    }
+
+    // Construct a map with the specified initial capacity and load factor
+    public MyHashMap(int initialCapacity, float loadFactorThreshold) {
+        if (initialCapacity > MAXIMUM_CAPACITY)
+            this.capacity = MAXIMUM_CAPACITY;
+        else
+            this.capacity = trimToPowerOf2(initialCapacity);
+
+        this.loadFactorThreshold = loadFactorThreshold;
+        table = new LinkedList[capacity];
+    }
+
+    @Override // Remove all of the entries from this map
+    public void clear() {
+        size = 0;
+        removeEntries();
+    }
+
+    @Override //Return true if the specified key is in the map
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+        
+    @Override // Return true if this map contains the value
+    public boolean containsValue(V value) {
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] != null) {
+                LinkedList<Entry<K, V>> bucket = table[i];
+                for (Entry<K, V> entry: bucket)
+                    if (entry.getValue().equals(value))
+                        return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        int bucketIndex = hash(key.hashCode());
+        if (table[bucketIndex] != null) {
+            LinkedList<Entry<K, V>> bucket = table[bucketIndex];
+            for (Entry<K, V> entry: bucket)
+                if (entry.getKey().equals(key))
+                    return entry.getValue(); 
+        }
+        return null;
+    }
+
+    @Override // Return true if this map contains no entries
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override //Return a set consisting of the keys in this map
+    public java.util.Set<K> keySet() {
+        java.util.Set<K> set = new java.util.HashSet<>();
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] != null) {
+                LinkedList<Entry<K, V>> bucket = table[i];
+                for (Entry<K, V> entry: bucket)
+                    set.add(entry.getKey());
+            }
+        }
+        return set;
+    }
+
+    @Override // Add an entry (key, value) into the map
+    public V put(K key, V value) {
+        if (get(key) != null) { // The key is already in the map
+            int bucketIndex = hash(key.hashCode());
+            LinkedList<Entry<K, V>> bucket = table[bucketIndex];
+            for (Entry<K, V> entry: bucket)
+                if (entry.getKey().equals(key)) {
+                    V oldValue = entry.getValue();
+                    entry.value = value; // Replace old value
+                    return oldValue;
+                }
+        }
+
+        // Add new entry
+        int bucketIndex = hash(key.hashCode());
+
+        if (table[bucketIndex] == null) {
+            table[bucketIndex] = new LinkedList<>();
+        }
+
+        table[bucketIndex].add(new Entry<>(key, value));
+        size++;
+
+        // Rehash if needed
+        if (size >= capacity * loadFactorThreshold) {
+            rehash();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void remove(K key) {
+        int bucketIndex = hash(key.hashCode());
+
+        if (table[bucketIndex] != null) {
+            LinkedList<Entry<K, V>> bucket = table[bucketIndex];
+
+            for (Entry<K, V> entry : bucket) {
+                if (entry.getKey().equals(key)) {
+                    bucket.remove(entry);
+                    size--;
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public java.util.Set<V> values() {
+        java.util.Set<V> set = new java.util.HashSet<>();
+
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] != null) {
+                for (Entry<K, V> entry : table[i]) {
+                    set.add(entry.getValue());
+                }
+            }
+        }
+        return set;
+    }
+
+    @Override
+    public java.util.Set<Entry<K, V>> entrySet() {
+        java.util.Set<Entry<K, V>> set = new java.util.HashSet<>();
+
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] != null) {
+                for (Entry<K, V> entry : table[i]) {
+                    set.add(entry);
+                }
+            }
+        }
+        return set;
+    }
+
+
+    private int hash(int hashCode) {
+        return hashCode & (capacity - 1);
+    }
+
+    private int trimToPowerOf2(int initialCapacity) {
+        int cap = 1;
+        while (cap < initialCapacity) {
+            cap <<= 1;
+        }
+        return cap;
+    }
+
+    private void removeEntries() {
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] != null) {
+                table[i].clear();
+            }
+        }
+    }
+
+    private void rehash() {
+        java.util.Set<Entry<K, V>> set = entrySet();
+
+        capacity <<= 1; // double capacity
+        table = new LinkedList[capacity];
+        size = 0;
+
+        for (Entry<K, V> entry : set) {
+            put(entry.getKey(), entry.getValue());
+        }
+    }
+}
